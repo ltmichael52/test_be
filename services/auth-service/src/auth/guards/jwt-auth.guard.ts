8e9935533327501860f54abe8c3371f780065919
lephat <ltmichael52@gmail.com>
2026-03-26 15:18:51 +0700

    123

--- services/auth-service/src/auth/guards/jwt-auth.guard.ts
+++ services/auth-service/src/auth/guards/jwt-auth.guard.ts
@@ -2,7 +2,7 @@ import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/com
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../auth.service';
 
-// test 23334
+// test 233344
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private readonly authService: AuthService) {
